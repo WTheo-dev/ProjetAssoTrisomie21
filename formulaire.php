5ca5e06126d8a7519f6b8dc089916552fdd7ce41
HamzaKage <112856766+HamzaKage@users.noreply.github.com>
2022-11-21 10:51:04 +0100

    Correction du code

--- formulaire.php
+++ formulaire.php
@@ -1,6 +1,6 @@
 <?php
 	try {
- 		$linkpdo = new PDO("mysql:host=localhost;dbname=testsae", 'root', '');
+ 		$linkpdo = new PDO("mysql:host=localhost;dbname=sae", 'root', '');
  	}
  	catch (Exception $e) {
  		die('Error : ' . $e->getMessage());
@@ -22,30 +22,24 @@
 	}
 
 	// SI IL TROUVE DES COMPTE AVEC MEME MOT DE PASSE OU LOGIN :
- 	if ($req->rowCount() > 0) {
- 		echo "J'AI TROUVE DES COMPTES AVEC MEME MOT DE PASSE OU LOGIN !";
-		echo $req->rowCount();
- 	}
- 	else {
-	 	$req = $linkpdo->prepare('INSERT INTO membre (Nom, Prénom, Adresse, Code_Postal, Ville, Courriel, Date_Naissance, Mdp) VALUES (:nom, :prenom, :adresse, :code_postal, :ville, :courriel, 
-		:date_de_naissance, :mot_de_passe)');
 
-	 	if ($req == false) {
-	 		die ('Error preparation');
-	 	}
+	$req = $linkpdo->prepare('INSERT INTO membre VALUES ("", :nom, :prenom, :adresse, :code_postal, :ville, :courriel, :date_de_naissance, :mot_de_passe, NULL, NULL)');
 
-	 	$req2 = $req->execute(array('nom' => $_POST["nom"],
-	 						'prenom' => $_POST["prenom"],
-							'adresse' => $_POST["adresse"],
-							'code_postal' => $_POST["cp"],
-							'ville' =>  $_POST["vil"],
-							'courriel' => $_POST["adresseE"],
-							'date_de_naissance' => $_POST["ddn"],
-							'mot_de_passe' => $_POST["mdp"]));
+	if ($req == false) {
+	 	die ('Error preparation');
+	}
 
-	 	if ($req2 == false) {
-	 		$req->DebugDumpParams();
-	 		die ('Error execute 2');
-	 	}
+	$req2 = $req->execute(array('nom' => $_POST["nom"],
+	 					'prenom' => $_POST["prenom"],
+						'adresse' => $_POST["adresse"],
+						'code_postal' => $_POST["cp"],
+						'ville' =>  $_POST["vil"],
+						'courriel' => $_POST["adresseE"],
+						'date_de_naissance' => $_POST["ddn"],
+						'mot_de_passe' => $_POST["mdp"]));
+
+	if ($req2 == false) {
+	 	$req->DebugDumpParams();
+	 	die ('Error execute 2');
 	}
 ?>
