6ce0efab6da510e1b3139b5e1f32df053c55adbd
Tatsu <guillaume.pomies82@gmail.com>
2023-01-16 11:19:48 +0100

    modif chat

--- chat.php
+++ chat.php
@@ -18,58 +18,129 @@ AND !empty ($_POST['message']))
         <meta charset="utf-8">
     </head>
     <body>
-      <?php
-        $allmsg = $bdd->query('SELECT * FROM chat ORDER BY id DESC');
-        while($msg = $allmsg->fetch())
-        {
+      <div id="chatHeader">
+        <p >CHAT</h1>
+      </div>
+      <div id="sentMsg">
+        <?php
+          $allmsg = $bdd->query('SELECT * FROM chat ORDER BY id DESC');
+          while($msg = $allmsg->fetch())
+          {
+            ?>
+          <div class="affichage">
+            <div id="sender"><?php echo $msg['nom']; ?> :</div>
+            <div id="msg"><?php echo $msg['message']; ?></div>
+          </div>
+          
+        <?php
+          }
           ?>
-        <div class="affichage">
-        <b><?php echo $msg['nom']; ?> : 
-        </b><?php echo $msg['message']; ?><br/>
-        </div>
-        
-      <?php
-        }
-        ?>
-        <form method="post" action="">
-            <input type="text" name="nom" placeholder="NOM" /><br />
-            <textarea type="text" name="message" placeholder="MESSAGE" cols="30"rows="10"></textarea><br/>
-    <button type="submit" value="Envoyer" />Envoyer</button>
-        </form>
+      </div>
+      <form id="formText" method="post" action="">
+          <input type="text" name="nom" placeholder="NOM" /><br />
+          <textarea type="text" name="message" placeholder="MESSAGE" cols="30"rows="10"></textarea><br/>
+          <button type="submit" value="Envoyer">Envoyer</button>
+      </form>
     </body>
 </html>
 <style> 
 
+@font-face {
+  font-family: Franchise;
+  src: url(Franchise-Free-Bold.otf);
+}
+
+body{
+  width:90%;
+  margin: auto;
+  padding-top : 20px;
+  display : flex;
+  flex-direction : column;
+  background: #34568B;
+}
+
+#chatHeader{
+  height: 10%;
+  width: 100%;
+  background: #92A8D1;  
+  text-align:center;
+  border-top-left-radius: 10px;
+  border-top-right-radius: 10px;
+  font-family: Franchise, Arial;
+  font-size: 30px;
+  padding-bottom:10px;
+  color:black;
+  border-top: 2px solid black;
+  border-left: 2px solid black;
+  border-right: 2px solid black;
+  border-bottom: 2px solid black;
+}
+
+#sentMsg{
+  height: 80%;
+  width: 100%;
+  background: white;
+  border-left: 2px solid black;
+  border-right: 2px solid black;
+  list-style-type:none;
+	overflow: auto;
+}
+
+#formText{
+  height: 10%;
+  width: 100%;
+  display:flex;
+  flex-direction:row;
+  border:2px solid black;
+  border-bottom-left-radius: 10px;
+  border-bottom-right-radius: 10px;
+}
+
+#formText input{
+  width:10%;
+  height:100%
+}
 
 input[type="text"],
 textarea {
-  border: 1px solid #ccc;
-  border-radius: 5px;
+  border: 1px solid black;
   padding: 8px;
   font-size: 16px;
-  width: 80%;
+  width:80%;
+  height:100%
   box-sizing: border-box;
+  resize:none;
  }
 
+
 button[type="submit"] {
-  background-color: lightblue;
+  background-color: #4CAF50;
   border: none;
-  border-radius: 5px;
   padding: 8px 16px;
   font-size: 16px;
   color: white;
   cursor: pointer;
+  width:10%;
+  height:100%;
 }
-b {
-  color: blue;
-  font-size: 16px;
-}
+
 .affichage {
-  background-color: #eee;
-  border: 1px solid #ccc;
-  border-radius: 5px;
-  padding: 10px;
-  /* margin-bottom: 10px; */
+
+  background-color: #6fbced;
+  padding: 10px; /* Ajoutez un peu de marge à l'intérieur de chaque container */
+  min-width: 50px; /* Il faut une taille minimale pour le container */
+  display: flex;
+  flex-direction:column;
+  width:fit-content;
+  border-radius : 5px;
+  margin : 5px;
+  font-family: Roboto,Open Sans,Arial;
+  color:white;
+}
+
+#sender{
+  font-size:15px;
+  font-weight: bold;
 }
 
 </style>
