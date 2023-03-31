10e2f06f4bbbbead476ebcc329c0c9d3de41d204
Theo <wazydrag.theo@gmail.com>
2023-03-31 09:02:34 +0200

    modif

--- Page_Principale/deconnexion.php
+++ Page_Principale/deconnexion.php
@@ -2,7 +2,7 @@
     session_start();
     if ($_SESSION[''] == 0){
         session_destroy();
-        header('Location: ');
+        header('Location:  ');
     } else{
         session_destroy();
         header('Location: ');
